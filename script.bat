7bb57a0837d4459157962aee1225c365dfd9ae25
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-11-06 19:02:09 +0100

    updated

--- script.bat
+++ script.bat
@@ -1,14 +1,14 @@
 @echo off
-echo Stopping PodcastMS
-docker stop PodcastMS
-echo Deleting container PodcastMS
-docker rm PodcastMS
-echo Deleting image PodcastMS
-docker rmi PodcastMS
+echo Stopping edufygateway
+docker stop edufygateway
+echo Deleting container edufygateway
+docker rm edufygateway
+echo Deleting image edufygateway
+docker rmi edufygateway
 echo Running mvn package (skips tests)
 call mvn package -DskipTests
-echo Creating image PodcastMS
-docker build -t PodcastMS .
-echo Creating and running container PodcastMS
-docker run -d -p 9904:9904 --name PodcastMS --network dufy_network PodcastMS
+echo Creating image edufygateway
+docker build -t edufygateway .
+echo Creating and running container edufygateway
+docker run -d -p 9901:9901 --name edufygateway --network edufy_network edufygateway
 echo Done!
--- src/main/java/com/example/edufygateway/EdufyGatewayApplication.java
+++ src/main/java/com/example/edufygateway/EdufyGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EdufyGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdufyGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyGatewayApplication.class, args);
+    }
 
 }
